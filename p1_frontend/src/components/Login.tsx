d65be023bdfdfcec1d78eec2c474a059b599d3e2
Kyle Stulen <62697063+KyleStulen-1@users.noreply.github.com>
2023-04-10 11:39:41 -0500

    actually pushing login

--- p1_frontend/src/components/Login.tsx
+++ p1_frontend/src/components/Login.tsx
@@ -1,6 +1,7 @@
 import { SyntheticEvent, useState } from "react";
 import { User } from "../models/user";
 import { Navigate } from "react-router-dom";
+import { authenticate } from "../remote/services/session-service";
 
 interface ILoginProps{
     currentUser: User | undefined,
@@ -8,7 +9,58 @@ interface ILoginProps{
 }
 
 export default function Login(props: ILoginProps){
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    let updateUserName = (e: SyntheticEvent) => {
+        setUserName((e.target as HTMLInputElement).value);
+    }
+
+    let updatePassword = (e: SyntheticEvent) => {
+        setPassword((e.target as HTMLInputElement).value);
+    }
+
+    let login = async() => {
+
+        if (userName && password) {
+            try {
+                let response = await authenticate({userName, password});
+
+                if (response.status === 201) {
+                    let data: User = response.data;
+                    props.setCurrentUser(data);
+                    sessionStorage.setItem('token', data.token);
+                } else {
+                    setErrorMessage('Credentials invalid');
+                }
+            } catch (err) {
+                setErrorMessage('Unable to connect to the API')
+            }
+        } else {
+          setErrorMessage('Invalid input for email/password')
+        }
+    }
+
     return (
-        <p>TODO: Login page here!</p>
+        props.currentUser ?
+            <>
+                <Navigate to="/home"/>
+            </>
+            :
+            <>
+                <p>Login to Revature Reimbursements app!</p>
+                <div>
+                    <input type="text" id="login-email" placeholder="Enter your email" onChange={updateUserName}/>
+                    <br /><br />
+                    <input type="text" id="login-password" placeholder="Enter your password" onChange={updatePassword}/>
+                    <br /><br />
+                    <button id="login-button" onClick={login}>Login</button>
+                </div>
+                <div>
+                    {errorMessage}
+                </div>
+            </>
     );
 }
